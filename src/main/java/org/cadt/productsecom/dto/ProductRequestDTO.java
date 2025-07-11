0c635b5119c54ee72894afd6e0076286fe222d3e
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-11 12:48:27 +0530

    product response DTO done

--- src/main/java/org/cadt/productsecom/dto/ProductRequestDTO.java
+++ src/main/java/org/cadt/productsecom/dto/ProductRequestDTO.java
@@ -3,7 +3,9 @@ package org.cadt.productsecom.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class ProductRequestDTO {
 
     @NotBlank(message = "Product name is required")
