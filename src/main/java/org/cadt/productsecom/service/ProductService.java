a2c87ccaee9eeec688e5fbf9ab4140ef393fdc24
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-07-11 12:53:57 +0530

    more updates - Service DTOS

--- src/main/java/org/cadt/productsecom/service/ProductService.java
+++ src/main/java/org/cadt/productsecom/service/ProductService.java
@@ -1,18 +1,19 @@
 package org.cadt.productsecom.service;
 
+import org.cadt.productsecom.dto.ProductRequestDTO;
 import org.cadt.productsecom.dto.ProductResponseDTO;
 import org.cadt.productsecom.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    ProductResponseDTO saveProduct(Product product);
+    ProductResponseDTO saveProduct(ProductRequestDTO productRequestDTO);
 
     List<ProductResponseDTO> getAllProducts();
 
     ProductResponseDTO getProductById(Long id);
 
-    ProductResponseDTO updateProduct(Long id, Product product);
+    ProductResponseDTO updateProduct(Long id, ProductRequestDTO productRequestDTO);
 
     void deleteProduct(Long id);
 }
